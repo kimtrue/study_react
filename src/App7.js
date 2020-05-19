509527ffdd329687367597b9b17e46251c39ff71
trueekim <kim_truee@naver.com>
2020-05-19 21:16:34 +0900

    state and LifeCycle

--- src/App7.js
+++ src/App7.js
@@ -1,8 +1,11 @@
 import React from 'react';
 /*
- data 에 fetch하기
+render를 하면 호출되는 
+Life cycle method는?
+ComponentDidmount
  */
 class App extends React.Component {
+  //미래에 사용할 state를 선언하는건 필수가 아니다
  state = {
    isLoading: true,
    movie: []
@@ -14,8 +17,10 @@ class App extends React.Component {
    }, 6000);
  }
 render(){
+  //ES6 문법
   const {isLoading} = this.state;
   return (
+    // <div>{this.state.isLoading ? "Loading" : "We are Ready"}</div>
     <div>{isLoading ? "Loading..." : "We are Ready"}</div>
     )
   }
